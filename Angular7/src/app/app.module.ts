d9896024ba926aaf7d7d7c1cb4c1a15fce2712ff
ramesh.babu <ramesh.babu@envoyglobal.com>
2019-09-23 19:48:43 -0500

    Adding Angular material

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GlobalNavComponent } from './global-nav/global-nav.component';
 import { InitialBookingModule } from './initial-booking/initial-booking.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { InitialBookingModule } from './initial-booking/initial-booking.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InitialBookingModule
+    InitialBookingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
