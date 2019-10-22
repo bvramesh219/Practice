54e688af03ddc54700f8dcf0aa8a3c58aec5d9d5
ramesh.babu <ramesh.babu@envoyglobal.com>
2019-10-21 20:30:20 -0500

    Created Shared Library

---
+++ Nrwl-Nx/nrwl-nx-workspace/libs/shared/src/lib/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountryListComponent } from './components/country-list/country-list.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CountryListComponent]
+})
+export class SharedModule {}
