e5524d5d3c6ecaf4b74387f2b5d1a3afcafd71c0
bvramesh219 <53973443+bvramesh219@users.noreply.github.com>
2020-07-29 09:55:45 -0500

    Some interview related samples

--- ES6-Examples/ES6/samples.js
+++ ES6-Examples/ES6/samples.js
@@ -1,6 +1,71 @@
 function runSamples() {
     console.log("Running Samples");
     sampleExamples();
+    interviewSamples();
+}
+
+
+function interviewSamples() {
+    function countingValleys(s) {
+        let valleyCount = 0;
+        let netVal = 0;
+        for(var i=0; i<s.length; i++){
+            if(s[i] === 'U'){
+                netVal++;
+                if(netVal === 0) {
+                    valleyCount++;
+                }
+            } else {
+                netVal--;
+            }
+        }
+        console.log(valleyCount);
+    }
+
+    countingValleys('UDDDUDUU');
+
+
+    function performSwap(initial, final) {
+        let totalBribes = 0;
+        let done = false;
+        while(!done) {
+            if(initial.length ==0) {
+                done = true;
+                break;
+            }
+            if(initial[0]!== final[0]) {
+                let swapedItem = final[0];
+                let indexInInitial = initial.indexOf(swapedItem);
+                let noOfBribes = indexInInitial;
+                if(noOfBribes > 2) {
+                    totalBribes = -1;
+                    done = true;
+                } else if(noOfBribes > 0){
+                    totalBribes = totalBribes + noOfBribes;
+                    let swapedArr= initial.slice();
+                    swapedArr.splice(indexInInitial,1);
+                    swapedArr.unshift(swapedItem);
+                    initial = swapedArr;
+                } else {
+                    continue;
+                }
+            }  else {
+                initial.shift();
+                final.shift();
+            }
+        }
+        return totalBribes;
+    }
+debugger;
+let q = [1,2,5,3,7,8,6,4,9];
+let p = q.slice();
+p = p.sort((a, b) => {
+    if(a!==b) {
+        return a > b ? 1 : -1;
+    } 
+    return 0;
+})
+    let swaps = performSwap(p, q );
 }
 
 function sampleExamples() {
@@ -62,7 +127,6 @@ function sampleExamples() {
    var nested = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
    console.log( nested.flat().flat().flat().flat());
 
-   debugger;
    
     let sockMerchant= function(n, ar) {
         let noOfPairs = 0;
